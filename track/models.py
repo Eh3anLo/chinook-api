27b771c746ffbf66a01a3f16eb4b1adedf69c0d9
vera <ehsunlotfy@gmail.com>
2024-08-11 16:27:07 +0330

    first commit

---
+++ track/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+
+# Create your models here.
+
+class MediaType(models.Model):
+    media_type_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=120, blank=True, null=True)
+
+    class Meta:
+        db_table = 'media_type'
+
+
+
+class Genre(models.Model):
+    genre_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=120, blank=True, null=True)
+
+    class Meta:
+        db_table = 'genre'
+
+
+
+class Album(models.Model):
+    album_id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=160)
+    artist = models.ForeignKey('Artist', models.DO_NOTHING)
+
+    class Meta:
+        db_table = 'album'
+
+
+
+class Track(models.Model):
+    track_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=200)
+    album = models.ForeignKey(Album, models.DO_NOTHING, blank=True, null=True)
+    media_type = models.ForeignKey(MediaType, models.DO_NOTHING)
+    genre = models.ForeignKey(Genre, models.DO_NOTHING, blank=True, null=True)
+    composer = models.CharField(max_length=220, blank=True, null=True)
+    milliseconds = models.IntegerField()
+    bytes = models.IntegerField(blank=True, null=True)
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        db_table = 'track'
+
+
+
+class Artist(models.Model):
+    artist_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=120, blank=True, null=True)
+
+    class Meta:
+        db_table = 'artist'
+
+
