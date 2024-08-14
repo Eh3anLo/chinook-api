f6b313290b5c75a02c90f987ac8c7de76d979927
ehsan <ehsunlotfy@gmail.com>
2024-08-14 15:36:43 +0330

    PlayList informaion API, fixed bugs of previews

--- playlists/serializers.py
+++ playlists/serializers.py
@@ -1,13 +1,26 @@
 from rest_framework import serializers
 from track.serializers import TrackSerializer
 from track.models import Track
-from .models import PlaylistTrack
-class PlaylistSerializer(serializers.Serializer):
-    playlist = serializers.StringRelatedField()
-    track = serializers.SerializerMethodField()
+from .models import Playlist, PlaylistTrack
 
-    def get_track(self, obj):
-        
-        
-        serializer = TrackSerializer(obj.track)
+class PlaylistSerializer(serializers.ModelSerializer):
+    total_tracks = serializers.SerializerMethodField()
+    tracks = serializers.SerializerMethodField()
+
+    def get_tracks(self, obj):
+        tracks_in_list = PlaylistTrack.objects.filter(playlist=obj.playlist_id).values_list('track_id', flat=True)
+        tracks = Track.objects.filter(track_id__in=set(tracks_in_list))
+        serializer = TrackSerializer(tracks, many=True)
         return serializer.data
+        
+
+    def get_total_tracks(self, obj):
+        tracks_count = PlaylistTrack.objects.filter(playlist=obj.playlist_id).count()
+        return tracks_count
+
+    class Meta:
+        model = Playlist
+        fields = ('playlist_id' , 'playlist_title', 'total_tracks','tracks')
+        extra_kwargs = {
+            'playlist_title' : {'source' : 'name'}
+        }
--- playlists/views.py
+++ playlists/views.py
@@ -1,17 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from track.models import Track
 from .serializers import PlaylistSerializer
-from .models import PlaylistTrack
+from .models import PlaylistTrack, Playlist
 # Create your views here.
 
 class PlaylistApiView(APIView):
 
     def get(self, request , pk):
-        playlist_queryset = PlaylistTrack.objects.filter(pk=pk).prefetch_related('track')
-        serializer = PlaylistSerializer(playlist_queryset, many=True)
+        playlist = get_object_or_404(Playlist, pk=pk)
+        serializer = PlaylistSerializer(playlist)
 
         return Response(serializer.data, status=status.HTTP_200_OK)
 
